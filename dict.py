1e72f41235b5717357d3ff2d63480b1f341250c1
aapadalko <a.padalko@gmail.com>
2020-06-14 04:06:53 +0300

    1

---
+++ dict.py
@@ -0,0 +1,5 @@
+me={'name':'Andrew','surname':'Padalko'}
+godfather1={'name':'Taras','surname':'Savchenko'}
+godfather2={'name':'Andrew','surname':'Nazarenko'}
+Friends={'me':me,'godfather1':godfather1,'godfather2':godfather2}
+print(Friends)
